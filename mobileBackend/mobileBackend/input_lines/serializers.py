275bf9d0d8472265f31dabb2981f5b86d5e91133
kangazoom <michellelcronin@gmail.com>
2019-01-04 15:24:23 -0800

    issues getting param query up and running; just restful routing now for api

--- mobileBackend/mobileBackend/input_lines/serializers.py
+++ mobileBackend/mobileBackend/input_lines/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from .models import InputLine
 
+# from django.contrib.auth import get_user_model
+from rest_framework.reverse import reverse
+# import django_filters
+
 class InputLineSerializer(ModelSerializer):
     class Meta:
         model = InputLine
-        fields = '__all__'
+        fields = ('id', 'title', 'author', 'description', 'speaking_character', 'user_character', 'line_text')
+
+    def get_status_display(self, obj):
+        return obj.get_status_display()
