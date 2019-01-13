3565d2908de9db137ae251382ed3bdf135d1262e
kangazoom <michellelcronin@gmail.com>
2019-01-13 00:28:35 -0800

    started over b/c i broke ... something ...

---
+++ runlines/ios/Pods/Target Support Files/Pods-runlines/Pods-runlines-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_runlines : NSObject
+@end
+@implementation PodsDummy_Pods_runlines
+@end
---
+++ runlines/src/Components/ChooseCharacter.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { StyleSheet, Button, View, Text, FlatList } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+class ChooseCharacter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            characterName: props.characterName
+        }
+    }
+
+    onCharacterPress = (characterName) => {
+        Actions.scriptContainer({ selectedCharacter: characterName, selectedScript: this.props.selectedScript })
+    }
+
+    render() {
+        const { selectedScript } = this.props;
+
+        let characterList = [... new Set(selectedScript.script_data.map((eachLine) => {
+            return eachLine['speaking_character']
+        }))];
+
+        return (
+            <View>
+                <Text>CHOOSE A CHARACTER:</Text>
+                <FlatList
+                    data={characterList}
+                    renderItem={({ item }) =>
+                        <Button
+                            title={`${item}`}
+                            onPress={() => {
+                                this.onCharacterPress(item)
+                            }}
+                        />
+                    }
+                    keyExtractor={item => item}
+
+                />
+            </View>
+        );
+
+    }
+}
+
+export default ChooseCharacter;
