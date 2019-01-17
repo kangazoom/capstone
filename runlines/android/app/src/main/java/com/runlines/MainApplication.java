392f78a491fdbaf0d78d1575d9c1e89fdc07cd63
kangazoom <michellelcronin@gmail.com>
2019-01-17 00:17:00 -0800

    at long last - the google speech transcription feature works/! I changed the encoding to mlaw/ulaw

--- runlines/android/app/src/main/java/com/runlines/MainApplication.java
+++ runlines/android/app/src/main/java/com/runlines/MainApplication.java
@@ -3,9 +3,9 @@ package com.runlines;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.rnfs.RNFSPackage;
-import com.rnim.rn.audio.ReactNativeAudioPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,9 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeAudioPackage(),
             new RNFetchBlobPackage(),
             new RNFSPackage(),
-            new ReactNativeAudioPackage(),
             new RNFirebasePackage()
       );
     }
--- runlines/src/Components/RecorderContainer.js
+++ runlines/src/Components/RecorderContainer.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+// import {AudioRecorder, AudioUtils} from 'react-native-audio';
+
+
 import {
   ScrollView,
   StyleSheet,
@@ -35,7 +38,8 @@ class RecorderContainer extends Component {
       isFinishRecorded: false,
       isPaused: false,
       currentTime: 0,
-      audioLength: 0
+      audioLength: 0,
+
     }
     this.timer = null
   }
@@ -47,11 +51,11 @@ class RecorderContainer extends Component {
   prepareRecordingPath() {
     console.log(Constants.AUDIO_PATH)
     AudioRecorder.prepareRecordingAtPath(Constants.AUDIO_PATH, {
-      SampleRate: 22050,
+      SampleRate: 16000,
       Channels: 1,
-      AudioQuality: 'Low',
-      AudioEncoding: 'aac',
-      AudioEncodingBitRate: 32000,
+      AudioQuality: 'high',
+      AudioEncoding: 'ulaw',
+      // AudioEncodingBitRate: 32000,
       // IncludeBase64: true,
     })
   }
@@ -104,16 +108,6 @@ class RecorderContainer extends Component {
   }
 
   encodeAudio = () => {
-    // let Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
-    // let encodedString = Base64.encode(Constants.AUDIO_PATH);
-    // return encodedString
-
-    // console.log(encodedString); // Outputs: "SGVsbG8gV29ybGQh"
-    // console.log(RNFetchBlob)
-    //     const Buffer = require("buffer").Buffer;
-    // let encodedAuth = new Buffer("your text").toString("base64");
-    // console.log(Buffer)
-
     console.log('k in encoding land')
     const filepath = Constants.AUDIO_PATH;
     const encoding = 'base64'
@@ -124,19 +118,15 @@ class RecorderContainer extends Component {
         console.log(typeof data)
         if(!data) { return Promise.reject('We got not data from readFile') }
         console.log('encoding successful')
-
-
-        // FireBaseService.initializeService()
-        // FireBaseService.addEncodedAudio(data)
-
         return GoogleSpeechService.discover(data);
     }).then((transcript) => {
       console.log(JSON.stringify(transcript));
+      this.returnedTranscriptionResponse(transcript)
       return Promise.resolve('yay encoded data')
-
     })
       .catch((err) => {
         console.log(`no encoding ${err}`)
+        console.log(this.sendTranscript)
         return Promise.reject('error')
       })
 
@@ -152,31 +142,8 @@ class RecorderContainer extends Component {
 
   }    
 
-  tryFunctions = () => {
-    let URL = "https://us-central1-run-lines.cloudfunctions.net/googleSpeechRequest"
-  //   let audio = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-  //   let config = {
-  //     "encoding": "FLAC",
-  //     "sampleRateHertz": 22050,
-  //     "languageCode": "en-US",
-  // }
-  //   let data = {
-  //     "audio": audio,
-  //     "config": config,
-  // }
-
-    axios.get(URL)
-    .then((response) => {
-      console.log(response)
-      return response
-    })
-    .catch((error) => {
-      console.log('NO FUNCIONS FOR YOU')
-      console.log(error)
-      return error
-    }
-    )
-
+  returnedTranscriptionResponse = (transcription) => {
+    this.props.returnedTranscriptionResponseCB(transcription)
   }
 
   render() {
